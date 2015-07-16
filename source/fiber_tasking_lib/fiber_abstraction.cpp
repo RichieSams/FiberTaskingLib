RichieSams/FiberTaskingLib

commit d6d1d524e0cb67d3e0c5a328d1e94b7dad82aea5

FIBER_TASKING_LIB: Only #include headers if needed

source/fiber_tasking_lib/fiber_abstraction.cpp
@@ -9,6 +9,8 @@
  * Copyright Adrian Astley 2015
  */
 
+#if defined(BOOST_CONTEXT)
+
 #include "fiber_tasking_lib/fiber_abstraction.h"
 
 #include "fiber_tasking_lib/tls_abstraction.h"
@@ -16,9 +18,9 @@
 
 namespace FiberTaskingLib {
 
-#if defined(BOOST_CONTEXT)
 // Boost.Context doesn't have a global way to get the current fiber
 TLS_VARIABLE(FiberId, tls_currentFiber);
-#endif
 
 } // End of namespace FiberTaskingLib
+
+#endif

commit 005982245909e10c4a1fbd591ce057337b1762ea

FIBER_TASKING_LIB: Use new fiber impl defines
This should address the regression introduced by d6d1d524e0cb67d3e0c5a328d1e94b7dad82aea5

source/fiber_tasking_lib/fiber_abstraction.cpp
@@ -9,10 +9,11 @@
  * Copyright Adrian Astley 2015
  */
 
-#if defined(BOOST_CONTEXT)
+#include "fiber_tasking_lib/config.h"
+
+#if defined(BOOST_CONTEXT_FIBER_IMPL)
 
 #include "fiber_tasking_lib/fiber_abstraction.h"
-
 #include "fiber_tasking_lib/tls_abstraction.h"
 
 

source/fiber_tasking_lib/fiber_abstraction.h
@@ -11,13 +11,14 @@
 
 #pragma once
 
+#include "fiber_tasking_lib/config.h"
 #include "fiber_tasking_lib/portability.h"
 #include "fiber_tasking_lib/tls_abstraction.h"
 
 #include <cstddef>
 
 
-#if defined(_MSC_VER)
+#if defined(WIN32_FIBER_IMPL)
 
 // VC++ implementation
 #define WIN32_LEAN_AND_MEAN
@@ -60,9 +61,8 @@ inline void FTLSetCurrentFiber(FiberType currentFiber) {
 
 } // End of namespace FiberTaskingLib
 
-#else
+#elif defined(BOOST_CONTEXT_FIBER_IMPL)
 // Boost.Context implementation
-#define BOOST_CONTEXT
 
 #include <boost/context/fcontext.hpp>
 #include <boost/context/fixedsize_stack.hpp>
@@ -138,5 +138,7 @@ inline void FTLSetCurrentFiber(FiberType currentFiber) {
 
 } // End of namespace FiberTaskingLib
 
+#else
+	#error This platform does not support fibers (currently). See fiber_tasking_lib/config.h
 #endif
 