d0d6c266244d1490642a00f031ca7bcdcc90d9b9
Adrian Astley <adastley@gmail.com>
2015-09-01 19:55:07 -0500

    CI: Fix copy paste fail

--- build_scripts/linux_setup.sh
+++ build_scripts/linux_setup.sh
@@ -5,12 +5,12 @@ sudo apt-get -qq update
 sudo apt-get -qq install gcc-5 g++-5 p7zip-full -y
 
 # Install latest boost
-  - curl https://files.adrianastley.com/programming/boost_1_58_0.tar.7z > boost_1_58_0.tar.7z
-  - 7z x boost_1_58_0.tar.7z > /dev/null
-  - tar xf boost_1_58_0.tar
-  - cd boost_1_58_0
-  - ./bootstrap.sh --with-libraries=context
-  - sudo ./b2 install > /dev/null
-  # Get back to the root
-  - cd ../
+curl https://files.adrianastley.com/programming/boost_1_58_0.tar.7z > boost_1_58_0.tar.7z
+7z x boost_1_58_0.tar.7z > /dev/null
+tar xf boost_1_58_0.tar
+cd boost_1_58_0
+./bootstrap.sh --with-libraries=context
+sudo ./b2 install > /dev/null
+# Get back to the root
+cd ../
 
