48d658441242f0d5089d480d655b30d4b632a80f
Adrian Astley <adastley@gmail.com>
2015-09-01 19:09:31 -0500

    CI: Add OSX build support to Travis CI

---
+++ build_scripts/linux_setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get -qq install gcc-5 g++-5 p7zip-full -y
+
+# Install latest boost
+  - curl https://files.adrianastley.com/programming/boost_1_58_0.tar.7z > boost_1_58_0.tar.7z
+  - 7z x boost_1_58_0.tar.7z > /dev/null
+  - tar xf boost_1_58_0.tar
+  - cd boost_1_58_0
+  - ./bootstrap.sh --with-libraries=context
+  - sudo ./b2 install > /dev/null
+  # Get back to the root
+  - cd ../
+
---
+++ build_scripts/osx_setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo brew update
+
+sudo brew tap homebrew/versions
+sudo brew install --enable-cxx gcc5
+
+brew install boost
