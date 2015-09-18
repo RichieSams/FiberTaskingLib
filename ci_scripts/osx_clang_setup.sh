12a8058d35d69d3e1bec18d72a14f6e1d17a53d5
RichieSams <adastley@gmail.com>
2015-09-18 11:02:49 -0500

    CI: Use pre-built clang binaries
    Since a homebrew build takes ~35 minutes

--- ci_scripts/osx_clang_setup.sh
+++ ci_scripts/osx_clang_setup.sh
@@ -2,11 +2,12 @@
 
 sudo brew update
 
-sudo brew tap homebrew/versions
-sudo brew -v install llvm --with-clang
+curl http://llvm.org/releases/3.7.0/clang+llvm-3.7.0-x86_64-apple-darwin.tar.xz > clang+llvm-3.7.tar.xz
+tar xzf clang+llvm-3.7.tar.xz
+cd clang+llvm-3.7
+sudo cp -R * /usr/local/
+cd ../
 
-clang --version
-
-export CXX="clang++-3.6" CC="clang-3.6"
+export CXX="clang++-3.7" CC="clang-3.7"
 
 brew outdated boost || brew upgrade boost
