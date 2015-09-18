3b6cb07549d5352cdc1d9fc0bab622b221988d2a
RichieSams <adastley@gmail.com>
2015-09-18 11:21:15 -0500

    CI: Run tar as root
    Since it will create a system link

--- ci_scripts/osx_clang_setup.sh
+++ ci_scripts/osx_clang_setup.sh
@@ -3,7 +3,7 @@
 sudo brew update
 
 curl http://llvm.org/releases/3.7.0/clang+llvm-3.7.0-x86_64-apple-darwin.tar.xz > clang+llvm-3.7.tar.xz
-tar xzf clang+llvm-3.7.tar.xz
+sudo tar xzf clang+llvm-3.7.tar.xz
 ls -l
 cd clang+llvm-3.7
 sudo cp -R * /usr/local/
