c070a64abf2eb9afb269c36defd17b6ee4354da7
Adrian Astley <adastley@gmail.com>
2015-09-18 01:19:12 -0500

    CI: Debug OSX hangs on make

--- build_scripts/osx_setup.sh
+++ build_scripts/osx_setup.sh
@@ -6,4 +6,4 @@ sudo brew tap homebrew/versions
 sudo brew install --enable-cxx gcc5
 sudo brew install llvm --with-clang --with-asan --HEAD
 
-brew outdated boost || brew upgrade boost > /dev/null
+brew outdated boost || brew upgrade boost
