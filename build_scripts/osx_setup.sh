07a00bd5a00bdf527a9dcdaa5e277942387ce3a2
Adrian Astley <adastley@gmail.com>
2015-09-18 01:07:37 -0500

    CI: Silence homebrew update

--- build_scripts/osx_setup.sh
+++ build_scripts/osx_setup.sh
@@ -6,4 +6,4 @@ sudo brew tap homebrew/versions
 sudo brew install --enable-cxx gcc5
 sudo brew install llvm --with-clang --with-asan --HEAD
 
-brew outdated boost || brew upgrade boost
+brew outdated boost || brew upgrade boost > /dev/null
