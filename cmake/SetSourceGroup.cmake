05a2b2562902cf23ea07168d0e02b2fe0440a613
Adrian Astley <adastley@gmail.com>
2016-01-16 19:00:20 -0600

    ALL: Completely reorganize the source and convert to pure CMAKE
    Closes #19 Closes #21

---
+++ cmake/SetSourceGroup.cmake
@@ -0,0 +1,19 @@
+macro( SetSourceGroup )
+	set(oneValueArgs NAME)
+	set(multiValuesArgs SOURCE_FILES)
+	cmake_parse_arguments(SRC_GROUP "" "${oneValueArgs}" "${multiValuesArgs}" ${ARGN} )
+	
+	string(REPLACE " " "_" VAR_SUFFIX "${SRC_GROUP_NAME}")
+	string(TOUPPER ${VAR_SUFFIX} VAR_SUFFIX)
+	
+	set(VAR_NAME SRC_${VAR_SUFFIX})
+	
+	set(${VAR_NAME}
+		${SRC_GROUP_SOURCE_FILES}
+	)
+	
+	string(REPLACE "Root" " " SRC_GROUP_NAME "${SRC_GROUP_NAME}")
+	string(REPLACE "root" " " SRC_GROUP_NAME "${SRC_GROUP_NAME}")
+	
+	source_group("${SRC_GROUP_NAME}" FILES ${${VAR_NAME}})
+endmacro()
