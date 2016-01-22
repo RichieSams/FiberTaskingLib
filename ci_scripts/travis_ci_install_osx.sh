95bcb2d795076656cfcd3b60d2bf92c36a909474
Adrian Astley <adastley@gmail.com>
2016-01-22 00:05:18 -0600

    CI: Force gcc 4.9 linking

--- ci_scripts/travis_ci_install_osx.sh
+++ ci_scripts/travis_ci_install_osx.sh
@@ -6,7 +6,7 @@ brew update
 
 case "${CXX}" in
 g++-4.8)     brew outdated gcc48 || brew upgrade gcc48 ;;
-g++-4.9)     brew install gcc49 ;;
+g++-4.9)     brew install gcc49 || brew link --overwrite gcc49;;
 g++-5)       brew install gcc5 ;;
 clang++-3.5) brew install llvm35 --with-clang ;;
 clang++-3.6) brew install llvm36 --with-clang ;;
