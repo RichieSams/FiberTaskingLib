3a4cf0efa740d6f3efd98f12c488e60756519a1b
Adrian Astley <adastley@gmail.com>
2016-01-21 23:43:41 -0600

    CI: Hardcode brew updates/installs
    The CI should change slow enough the hardcoding
    should be ok.

--- ci_scripts/travis_ci_install_osx.sh
+++ ci_scripts/travis_ci_install_osx.sh
@@ -3,25 +3,14 @@
 set -e
 
 brew update
-brew tap homebrew/versions
-
-install_brew_package() {
-  if [[ brew list "$1" ]] ; then
-    # Package is installed, upgrade if needed
-    brew outdated "$1" || brew upgrade "$1"
-  else
-    # Package not installed yet, install.
-    brew install "$1"
-  fi
-}
 
 case "${CXX}" in
-g++-4.8)     install_brew_package homebrew/versions/gcc48 ;;
-g++-4.9)     install_brew_package homebrew/versions/gcc49 ;;
-g++-5)       install_brew_package homebrew/versions/gcc5 ;;
-clang++-3.5) install_brew_package homebrew/versions/llvm35 --with-clang ;;
-clang++-3.6) install_brew_package homebrew/versions/llvm36 --with-clang ;;
+g++-4.8)     brew outdated gcc48 || brew upgrade gcc48 ;;
+g++-4.9)     brew install gcc49 ;;
+g++-5)       brew install gcc5 ;;
+clang++-3.5) brew install llvm35 --with-clang ;;
+clang++-3.6) brew install llvm36 --with-clang ;;
 *) echo "Compiler not supported: ${CXX}. See travis_ci_install_osx.sh"; exit 1 ;;
 esac
 
-install_brew_package boost
+brew outdated boost || brew upgrade boost
