0fe70c14652a72929e58bb7bbced884d589a28cf
Adrian Astley <adastley@gmail.com>
2016-01-24 22:32:36 -0600

    FIBER_TASKING_LIB: Use Win32 and pthread Read-Write lock
    instead of my own implementation.

--- include/fiber_tasking_lib/read_write_lock.h
+++ include/fiber_tasking_lib/read_write_lock.h
@@ -11,55 +11,105 @@
 
 #pragma once
 
-#include "fiber_tasking_lib/typedefs.h"
+#include "fiber_tasking_lib/config.h"
 
-#include <atomic>
-#include <mutex>
 
+
+
+#ifdef FTL_OS_WINDOWS
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
 
 namespace FiberTaskingLib {
 
 class ReadWriteLock {
 public:
-	ReadWriteLock();
-	~ReadWriteLock();
+	ReadWriteLock()
+		: m_lock(SRWLOCK_INIT) {
+	}
 
 private:
-	std::mutex m_writeLock;
-
-	std::atomic_uint m_readerCount;
+	SRWLOCK m_lock;
 
 public:
 	/**
 	 * Checks for a write lock. If none exists, increments the reader count
 	 */
-	void LockRead();
+	void LockRead() {
+		AcquireSRWLockShared(&m_lock);
+	}
 	/**
 	 * Decrements the reader count.
 	 */
-	void UnlockRead();
+	void UnlockRead() {
+		ReleaseSRWLockShared(&m_lock);
+	}
 
 	/**
 	 * Acquires a write lock. Then waits until reader count == 0
 	 */
-	void LockWrite();
+	void LockWrite() {
+		AcquireSRWLockExclusive(&m_lock);
+	}
 	/**
 	 * Releases the write lock
 	 */
-	void UnlockWrite();
-
-	/**
-	 * Tries to acquire a write lock. If it fails, returns false
-	 * If successful, it waits until reader count == 1, assuming the
-	 * last read lock is its own. If this is not the case, the last
-	 * reader will have undefined behavior.
-	 *
-	 * NOTE: This function does not release the read lock. It must be released
-	 *       as normal with UnlockRead()
-	 *
-	 * @return    Whether the upgrade was successful
-	 */
-	bool TryUpgradeReadToWriteLock();
+	void UnlockWrite() {
+		ReleaseSRWLockExclusive(&m_lock);
+	}
 };
 
 } // End of namespace FiberTaskingLib
+
+#elif defined(FTL_OS_LINUX) || defined(FTL_OS_OSX) || defined(FTL_OS_iOS)
+
+#include <pthread.h>
+
+namespace FiberTaskingLib {
+
+class ReadWriteLock {
+public:
+	ReadWriteLock()
+		: m_lock(PTHREAD_RWLOCK_INITIALIZER) {
+	}
+	~ReadWriteLock() {
+		pthread_rwlock_destroy(&m_lock);
+	}
+
+private:
+	pthread_rwlock_t m_lock;
+
+public:
+	/**
+	* Checks for a write lock. If none exists, increments the reader count
+	*/
+	void LockRead() {
+		pthread_rwlock_rdlock(&m_lock);
+	}
+	/**
+	* Decrements the reader count.
+	*/
+	void UnlockRead() {
+		pthread_rwlock_unlock(&m_lock);
+	}
+
+	/**
+	* Acquires a write lock. Then waits until reader count == 0
+	*/
+	void LockWrite() {
+		pthread_rwlock_wrlock(&m_lock);
+	}
+	/**
+	* Releases the write lock
+	*/
+	void UnlockWrite() {
+		pthread_rwlock_unlock(&m_lock);
+	}
+};
+
+} // End of namespace FiberTaskingLib
+
+#endif
+
+
