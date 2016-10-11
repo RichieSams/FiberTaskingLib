8c7fd706805a03792c7dceb3ddd86431205d886f
Adrian Astley <adastley@gmail.com>
2016-10-10 20:25:07 -0500

    Automatically upgrade / install brew formula

--- ci_scripts/travis_ci_install_osx.sh
+++ ci_scripts/travis_ci_install_osx.sh
@@ -3,28 +3,34 @@ set -x #echo on
 
 brew update
 
+upgradeBrewFormula () {
+	if brew ls --versions $1 > /dev/null
+	then
+		brew outdated $1 || brew upgrade $1
+	else
+		brew install $1
+	fi
+}
+
+
 case "${CXX}" in
 g++-4.8)
-	brew outdated gcc48 || brew upgrade gcc48
-	# Force a relink
-	brew link --overwrite gcc48
+	upgradeBrewFormula gcc48
 	;;
 g++-4.9)
-	brew outdated gcc49 || brew upgrade gcc49
-	# Force a re-link
-	brew link --overwrite gcc49
+	upgradeBrewFormula gcc49
 	;;
 g++-5)
-	brew outdated gcc5 || brew install gcc5
+	upgradeBrewFormula gcc5
 	;;
 clang++-3.5)
-	brew outdated llvm35 || brew install llvm35
+	upgradeBrewFormula llvm35
 	;;
 clang++-3.6)
-	brew outdated llvm36 || brew install llvm36
+	upgradeBrewFormula llvm36
 	;;
 clang++-3.7)
-	brew outdated llvm37 || brew install llvm37
+	upgradeBrewFormula llvm37
 	;;
 *) echo "Compiler not supported: ${CXX}. See travis_ci_install_osx.sh"; exit 1 ;;
 esac
