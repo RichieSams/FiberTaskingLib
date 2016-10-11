15eee820b65ae99ca77e7eaac4981f93746433c4
Adrian Astley <adastley@gmail.com>
2016-10-10 19:55:28 -0500

    Correct OSX CI script

--- ci_scripts/travis_ci_install_osx.sh
+++ ci_scripts/travis_ci_install_osx.sh
@@ -4,11 +4,11 @@ set -x #echo on
 brew update
 
 case "${CXX}" in
-g++-4.8)     brew outdated homebrew/versions/gcc48 || brew upgrade homebrew/versions/gcc48 ;;
-g++-4.9)     brew install homebrew/versions/gcc49 || brew link --overwrite homebrew/versions/gcc49 ;;
-g++-5)       brew install homebrew/versions/gcc5 ;;
-clang++-3.5) brew install homebrew/versions/llvm35 ;;
-clang++-3.6) brew install homebrew/versions/llvm36;;
-clang++-3.7) brew install homebrew/versions/llvm37;;
+g++-4.8)     brew outdated gcc48 || brew upgrade gcc48 ;;
+g++-4.9)     brew outdated gcc49 || brew upgrade gcc49 ;;
+g++-5)       brew install gcc5 ;;
+clang++-3.5) brew install llvm35 ;;
+clang++-3.6) brew install llvm36;;
+clang++-3.7) brew install llvm37;;
 *) echo "Compiler not supported: ${CXX}. See travis_ci_install_osx.sh"; exit 1 ;;
 esac
