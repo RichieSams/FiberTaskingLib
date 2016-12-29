e764c02034acda8c6869067efe1ca7b945be7183
Adrian Astley <adastley@gmail.com>
2016-12-29 15:16:07 -0600

    Add support for Valgrind and stack guards

--- include/fiber_tasking_lib/fiber.h
+++ include/fiber_tasking_lib/fiber.h
@@ -11,18 +11,54 @@
 
 #pragma once
 
+#include "fiber_tasking_lib/config.h"
+
 #include <boost_context/fcontext.h>
 
+#include <cassert>
 #include <cstdlib>
 #include <algorithm>
 
-//#include <valgrind/valgrind.h>
+#if defined(FTL_VALGRIND)
+	#include <valgrind/valgrind.h>
+#endif
+
+#if defined(FTL_FIBER_STACK_GUARD_PAGES)
+	#if defined(FTL_OS_LINUX) || defined(FTL_OS_MAC) || defined(FTL_iOS)
+		#include <sys/mman.h>
+		#include <unistd.h>
+	#elif defined(FTL_OS_WINDOWS)
+		#define WIN32_LEAN_AND_MEAN
+		#include <Windows.h>
+	#endif
+#endif
 
 
 namespace FiberTaskingLib {
 
+#if defined(FTL_VALGRIND)
+	#define FTL_VALGRIND_ID uint m_stackId
+
+	#define FTL_VALGRIND_REGISTER(s, e) \
+		m_stackId = VALGRIND_STACK_REGISTER(s, e)
+
+	#define SEW_VALGRIND_DEREGISTER() VALGRIND_STACK_DEREGISTER(m_stackId)
+#else
+	#define FTL_VALGRIND_ID
+	#define FTL_VALGRIND_REGISTER(s, e)
+	#define FTL_VALGRIND_DEREGISTER()
+#endif
+
+
+inline void MemoryGuard(void *memory, size_t bytes);
+inline void MemoryGuardRelease(void *memory, size_t bytes);
+inline std::size_t SystemPageSize();
+inline void *AlignedAlloc(std::size_t size, std::size_t alignment);
+inline void AlignedFree(void *block);
+inline std::size_t RoundUp(std::size_t numToRound, std::size_t multiple);
+
 typedef void (*FiberStartRoutine)(std::intptr_t arg);
-#define FTL_STACK_GUARD 1024
+
 
 class Fiber {
 public:
@@ -32,42 +68,56 @@ public:
 	 */
 	Fiber()
 		: m_stack(nullptr),
+		  m_systemPageSize(0),
 		  m_stackSize(0),
-		  m_context(nullptr), 
+		  m_context(nullptr),
 		  m_arg(0) {
 	}
 	/**
 	 * Allocates a stack and sets it up to start executing 'startRoutine' when first switched to
 	 *
-	 * @param stackSize       The stack size for the fiber
-	 * @param startRoutine    The function to run when the fiber first starts
-	 * @param arg             The argument to pass to 'startRoutine'
+	 * @param stackSize        The stack size for the fiber. If guard pages are being used, this will be rounded up to the next multiple of the system page size
+	 * @param startRoutine     The function to run when the fiber first starts
+	 * @param arg              The argument to pass to 'startRoutine'
 	 */
 	Fiber(std::size_t stackSize, FiberStartRoutine startRoutine, std::intptr_t arg)
-			: m_stackSize(stackSize),
-			  m_arg(arg) {
-		m_stack = malloc(stackSize);
-		m_context = boost_context::make_fcontext(static_cast<char *>(m_stack) + stackSize, stackSize, startRoutine);
+			: m_arg(arg) {
+		#if defined(FTL_FIBER_STACK_GUARD_PAGES)
+			m_systemPageSize = SystemPageSize();
+		#else
+			m_systemPageSize = 0;
+		#endif
+
+		m_stackSize = RoundUp(stackSize, m_systemPageSize);
+		// We add a guard page both the top and the bottom of the stack
+		m_stack = AlignedAlloc(m_systemPageSize + m_stackSize + m_systemPageSize, m_systemPageSize);
+		m_context = boost_context::make_fcontext(static_cast<char *>(m_stack) + m_systemPageSize + stackSize, stackSize, startRoutine);
+		
+		FTL_VALGRIND_REGISTER(static_cast<char *>(m_stack) + m_systemPageSize + stackSize, static_cast<char *>(m_stack) + m_systemPageSize);
+		#if defined(FTL_FIBER_STACK_GUARD_PAGES)
+			MemoryGuard(static_cast<char *>(m_stack), m_systemPageSize);
+			MemoryGuard(static_cast<char *>(m_stack) + m_systemPageSize + stackSize, m_systemPageSize);
+		#endif
 	}
-	
+
 	/**
 	 * Deleted copy constructor
-	 * It makes no sense to copy a stack and its corresponding context. Therefore, we explicitly forbid it. 
+	 * It makes no sense to copy a stack and its corresponding context. Therefore, we explicitly forbid it.
 	 */
 	Fiber(const Fiber &other) = delete;
 	/**
 	 * Move constructor
 	 * This does a swap() of all the member variables
 	 *
-	 * @param other    
+	 * @param other
 	 *
-	 * @return    
+	 * @return
 	 */
-	Fiber(Fiber &&other) 
+	Fiber(Fiber &&other)
 			: Fiber() {
 		swap(*this, other);
 	}
-	
+
 	/**
 	 * Move assignment operator
 	 * This does a swap() of all the member variables
@@ -80,31 +130,24 @@ public:
 		return *this;
 	}
 	~Fiber() {
-		free(m_stack);
-	}
+		if (m_stack != nullptr) {
+			if (m_systemPageSize != 0) {
+				MemoryGuardRelease(static_cast<char *>(m_stack), m_systemPageSize);
+				MemoryGuardRelease(static_cast<char *>(m_stack) + m_systemPageSize + m_stackSize, m_systemPageSize);
+			}
+			FTL_VALGRIND_DEREGISTER();
 
-private:
-	/**
-	 * Helper function for the move operators
-	 * Swaps all the member variables
-	 *
-	 * @param first     The first fiber
-	 * @param second    The second fiber
-	 */
-	void swap(Fiber &first, Fiber &second) {
-		using std::swap;
-
-		swap(first.m_stack, second.m_stack);
-		swap(first.m_stackSize, second.m_stackSize);
-		swap(first.m_context, second.m_context);
-		swap(first.m_arg, second.m_arg);
+			AlignedFree(m_stack);
+		}
 	}
 
 private:
 	void *m_stack;
+	std::size_t m_systemPageSize;
 	std::size_t m_stackSize;
 	boost_context::fcontext_t m_context;
 	std::intptr_t m_arg;
+	FTL_VALGRIND_ID;
 
 public:
 	/**
@@ -113,7 +156,7 @@ public:
 	 *
 	 * @param fiber    The fiber to switch to
 	 */
-	inline void SwitchToFiber(Fiber *fiber) {
+	void SwitchToFiber(Fiber *fiber) {
 		boost_context::jump_fcontext(&m_context, fiber->m_context, fiber->m_arg, 1);
 	}
 	/**
@@ -125,12 +168,130 @@ public:
 	 * @param startRoutine    The function to run when the fiber is next switched to
 	 * @param arg             The arg for 'startRoutine'
 	 *
-	 * @return    
+	 * @return
 	 */
-	inline void Reset(FiberStartRoutine startRoutine, std::intptr_t arg) {
+	void Reset(FiberStartRoutine startRoutine, std::intptr_t arg) {
 		m_context = boost_context::make_fcontext(static_cast<char *>(m_stack) + m_stackSize, m_stackSize, startRoutine);
 		m_arg = arg;
 	}
+	
+private:
+	/**
+	* Helper function for the move operators
+	* Swaps all the member variables
+	*
+	* @param first     The first fiber
+	* @param second    The second fiber
+	*/
+	void swap(Fiber &first, Fiber &second) {
+		using std::swap;
+
+		swap(first.m_stack, second.m_stack);
+		swap(first.m_systemPageSize, second.m_systemPageSize);
+		swap(first.m_stackSize, second.m_stackSize);
+		swap(first.m_context, second.m_context);
+		swap(first.m_arg, second.m_arg);
+	}
 };
+
+#if defined(FTL_FIBER_STACK_GUARD_PAGES)
+	#if defined(FTL_OS_LINUX) || defined(FTL_OS_MAC) || defined(FTL_iOS)
+		inline void MemoryGuard(void *memory, size_t bytes) {
+			int result = mprotect(memory, bytes, PROT_NONE);
+			if(result) {
+				perror("mprotect failed with error:");
+				assert(!result);
+			}
+		}
+
+		inline void MemoryGuardRelease(void *memory, size_t bytes) {
+			int result = mprotect(memory, bytes, PROT_READ | PROT_WRITE);
+			if(result) {
+				perror("mprotect failed with error:");
+				assert(!result);
+			}
+		}
+
+		inline std::size_t SystemPageSize() {
+			int pageSize = getpagesize();
+			return pageSize;
+		}
+
+		inline void *AlignedAlloc(std::size_t size, std::size_t alignment) {
+			void *returnPtr;
+			posix_memalign(&returnPtr, alignment, size);
+
+			return returnPtr;
+		}
+
+		inline void AlignedFree(void *block) {
+			free(block);
+		}
+	#elif defined(FTL_OS_WINDOWS)
+		inline void MemoryGuard(void *memory, size_t bytes) {
+			DWORD ignored;
+
+			BOOL result = VirtualProtect(memory, bytes, PAGE_NOACCESS, &ignored);
+			assert(result);
+		}
+
+		inline void MemoryGuardRelease(void *memory, size_t bytes) {
+			DWORD ignored;
+
+			BOOL result = VirtualProtect(memory, bytes, PAGE_READWRITE, &ignored);
+			assert(result);
+		}
+
+		inline std::size_t SystemPageSize() {
+			SYSTEM_INFO sysInfo;
+			GetSystemInfo(&sysInfo);
+			return sysInfo.dwPageSize;
+		}
+
+		inline void *AlignedAlloc(std::size_t size, std::size_t alignment) {
+			return _aligned_malloc(size, alignment);
+		}
+
+		inline void AlignedFree(void *block) {
+			_aligned_free(block);
+		}
+	#else
+		#error "Need a way to protect memory for this platform".
+	#endif
+#else
+	inline void MemoryGuard(void *memory, size_t bytes) {
+		(void)memory;
+		(void)bytes;
+	}
+
+	inline void MemoryGuardRelease(void *memory, size_t bytes) {
+		(void)memory;
+		(void)bytes;
+	}
+
+	inline std::size_t SystemPageSize() {
+		return 0;
+	}
+
+	inline void *AlignedAlloc(std::size_t size, std::size_t alignment) {
+		return malloc(size);
+	}
+
+	inline void AlignedFree(void *block) {
+		free(block);
+	}
+#endif
+
+inline std::size_t RoundUp(std::size_t numToRound, std::size_t multiple) {
+	if (multiple == 0) {
+		return numToRound;
+	}
+
+	std::size_t remainder = numToRound % multiple;
+	if (remainder == 0)
+		return numToRound;
+
+	return numToRound + multiple - remainder;
+}
 
 } // End of namespace FiberTaskingLib
