a873c5f0baa12ac4b0d2a9743acb69a90f34b103
Adrian Astley <adastley@gmail.com>
2017-06-23 09:36:48 -0500

    Add gcc-6 and clang-3.9 to CI test matrix

--- ci_scripts/travis_ci_install_osx.sh
+++ ci_scripts/travis_ci_install_osx.sh
@@ -23,11 +23,17 @@ g++-4.9)
 g++-5)
 	upgradeBrewFormula gcc5
 	;;
+g++-6)
+	upgradeBrewFormula gcc6
+	;;
 clang++-3.7)
 	upgradeBrewFormula llvm37
 	;;
 clang++-3.8)
 	upgradeBrewFormula llvm38
+	;;
+clang++-3.9)
+	upgradeBrewFormula llvm39
 	;;
 *) echo "Compiler not supported: ${CXX}. See travis_ci_install_osx.sh"; exit 1 ;;
 esac
