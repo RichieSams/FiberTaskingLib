34dc6f1ed11397b35cfee5b627494bcdefc1d622
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2018-10-29 01:27:47 -0400

    Fix store/fetchadd within AddTask

--- source/task_scheduler.cpp
+++ source/task_scheduler.cpp
@@ -325,7 +325,7 @@ void TaskScheduler::Run(uint const fiberPoolSize, TaskFunction const mainTask, v
 
 void TaskScheduler::AddTask(Task const task, AtomicCounter *const counter) {
 	if (counter != nullptr) {
-		counter->Store(1);
+		counter->FetchAdd(1, std::memory_order_acq_rel);
 	}
 
 	const TaskBundle bundle = {task, counter};
@@ -348,7 +348,7 @@ void TaskScheduler::AddTask(Task const task, AtomicCounter *const counter) {
 
 void TaskScheduler::AddTasks(uint const numTasks, Task const *const tasks, AtomicCounter *const counter) {
 	if (counter != nullptr) {
-		counter->Store(numTasks);
+		counter->FetchAdd(numTasks, std::memory_order_acq_rel);
 	}
 
 	ThreadLocalStorage &tls = m_tls[GetCurrentThreadIndex()];
