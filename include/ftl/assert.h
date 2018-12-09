dd76834b82e11dae9b5bd731c76f74745291fcdc
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2018-12-09 03:57:10 -0500

    Add cleaner assert macro

---
+++ include/ftl/assert.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+
+#define FTL_STR(s) #s
+#ifndef NDEBUG
+#	define FTL_ASSERT(msg, expr)                                                                                      \
+		if (!(expr)) {                                                                                                 \
+			std::cerr << "FTL Assertion Failure: " << (msg) << ". Expr: " FTL_STR(expr) "\n";                          \
+			std::cerr << "Source File: " __FILE__ ":" << __LINE__ << "\n";                                             \
+			std::abort();                                                                                              \
+		}
+#else
+#	define FTL_ASSERT(msg, expr)
+#endif
