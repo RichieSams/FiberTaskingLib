cf56bebfe307115c8e5eae6be5be1f37578c68e0
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2018-12-10 00:20:05 -0500

    Use c i/o functions with FTL_ASSERT

--- include/ftl/assert.h
+++ include/ftl/assert.h
@@ -1,13 +1,16 @@
 #pragma once
 
+#include <cstdio>
 #include <iostream>
 
-#define FTL_STR(s) #s
+#define FTL_EXPAND(s) s
+#define FTL_STR_IMPL(s) #s
+#define FTL_STR(s) FTL_EXPAND(FTL_STR_IMPL(s))
 #ifndef NDEBUG
 #	define FTL_ASSERT(msg, expr)                                                                                                          \
 		if (!(expr)) {                                                                                                                     \
-			std::cerr << "FTL Assertion Failure: " << (msg) << ". Expr: " FTL_STR(expr) "\n";                                              \
-			std::cerr << "Source File: " __FILE__ ":" << __LINE__ << "\n";                                                                 \
+			std::fputs("FTL Assertion Failure: " FTL_STR(msg) ". Expr: " FTL_STR(expr) "\n", stderr);                                      \
+			std::fputs("Source File: " __FILE__ ":" FTL_STR(__LINE__) "\n", stderr);                                                               \
 			std::abort();                                                                                                                  \
 		}
 #else
