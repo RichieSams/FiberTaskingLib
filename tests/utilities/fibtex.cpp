c4009c54d9a682f6c53afb9a59ed68a3238e8d92
RichieSams <adastley@gmail.com>
2020-03-09 08:28:06 -0700

    Fix compiler warnings

--- tests/utilities/fibtex.cpp
+++ tests/utilities/fibtex.cpp
@@ -64,7 +64,9 @@ void InfiniteSpinLockGuardTest(ftl::TaskScheduler * /*scheduler*/, void *arg) {
 
 TEST_CASE("Fibtex Locking Tests", "[utility]") {
 	ftl::TaskScheduler taskScheduler;
-	REQUIRE(taskScheduler.Init(ftl::TaskSchedulerInitOptions{400, 0, ftl::EmptyQueueBehavior::Yield}) == 0);
+	ftl::TaskSchedulerInitOptions options;
+	options.Behavior = ftl::EmptyQueueBehavior::Yield;
+	REQUIRE(taskScheduler.Init(options) == 0);
 
 	MutexData md(&taskScheduler);
 
