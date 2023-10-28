9ae6980190c73c5301e9c6c0a160e6aba16faab3
Adrian Astley <adastley@gmail.com>
2023-10-27 22:58:40 -0400

    Use AlignedAlloc for ThreadLocal
    Windows is complaining that new[] is using _aligned_alloc and delete[] is using free. So we just make it explicit

---
+++ include/ftl/alloc.h
@@ -0,0 +1,37 @@
+/**
+ * FiberTaskingLib - A tasking library that uses fibers for efficient task switching
+ *
+ * This library was created as a proof of concept of the ideas presented by
+ * Christian Gyrling in his 2015 GDC Talk 'Parallelizing the Naughty Dog Engine Using Fibers'
+ *
+ * http://gdcvault.com/play/1022186/Parallelizing-the-Naughty-Dog-Engine
+ *
+ * FiberTaskingLib is the legal property of Adrian Astley
+ * Copyright Adrian Astley 2015 - 2018
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <stddef.h>
+
+namespace ftl {
+
+void MemoryGuard(void *memory, size_t bytes);
+void MemoryGuardRelease(void *memory, size_t bytes);
+size_t SystemPageSize();
+void *AlignedAlloc(size_t size, size_t alignment);
+void AlignedFree(void *block);
+
+} // End of namespace ftl
